8bd96d9aef0ec2cfa0cb400fb88e497613b468a9
dmauney <dmauney@gmail.com>
2015-05-18 09:07:52 -0400

    NM scripts
    NM scripts

---
+++ scripts/msxls/extract_thresholds_nm.bat
@@ -0,0 +1,5 @@
+msxsl.exe -o BergdorfGoodman.txt BergdorfGoodman.profile.xml extract_thresholds.xsl
+msxsl.exe -o Horchow.txt Horchow.profile.xml extract_thresholds.xsl
+msxsl.exe -o LastCall.txt LastCall.profile.xml extract_thresholds.xsl
+msxsl.exe -o NeimanMarcus.txt NeimanMarcus.profile.xml extract_thresholds.xsl
+pause
